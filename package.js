ac63ab7d71abf27ef25eec46ff29e0c75be03e88
pointbar <stephane.langlois@scopyleft.fr>
2015-02-14 16:36:09 +0100

    Fix basic authentification

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'pntbr:github-md',
   summary: "A basic package to retrieve markdown ressources in a github repository",
-  version: "0.0.5",
+  version: "0.0.6",
   git: "https://github.com/pointbar/meteor-github-md.git"
 });
 
